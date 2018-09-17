e15a91989da1b816d4685570e072aff7e96874b8
schroeds1699 <43349036+schroeds1699@users.noreply.github.com>
2018-09-17 12:34:34 -0400

    Add files via upload
    first submission.

---
+++ P2T1_SchroederScott.py
@@ -0,0 +1,16 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/scott/AppData/Local/Programs/Python/Python37-32/sales prodection.py 
+enter the projected sales:1000
+the profit is $ 230.0
+>>> 
+>>> 
+ RESTART: C:/Users/scott/AppData/Local/Programs/Python/Python37-32/sales prodection.py 
+enter the projected sales:1000
+the profit is $ 230.00
+>>> 
+ RESTART: C:/Users/scott/AppData/Local/Programs/Python/Python37-32/sales prodection.py 
+enter the projected sales:10000
+the profit is $ 2,300.00
+>>> 
