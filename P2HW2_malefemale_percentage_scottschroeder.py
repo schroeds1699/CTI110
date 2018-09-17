dfa0b8a99115fc4ba531c744af710f1269e1577f
schroeds1699 <43349036+schroeds1699@users.noreply.github.com>
2018-09-17 14:14:03 -0400

    Add files via upload

---
+++ P2HW2_malefemale_percentage_scottschroeder.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/scott/AppData/Local/Programs/Python/Python37-32/CTI-110 P2HW2- male female percentage.py 
+ please enter the number of males in the class: 8
+ please enter the number of females in the class: 12
+male percentage: 40.0%
+female percentage:60.0%
+>>> 
