f20e8f5911e0ae82310e7287ec100ada82d47c4c
schroeds1699 <43349036+schroeds1699@users.noreply.github.com>
2018-09-17 13:29:17 -0400

    Add files via upload

---
+++ P2HW1_celsiusconverter_schroederscott.py
@@ -0,0 +1,7 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/scott/AppData/Local/Programs/Python/Python37-32/P2HW1_celsiusconverter_schroederscott.py 
+please enter a temperature in celsius 22
+71.6
+>>> 
